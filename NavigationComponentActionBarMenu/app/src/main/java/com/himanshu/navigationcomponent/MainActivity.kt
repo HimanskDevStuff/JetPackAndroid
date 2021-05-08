e65efdc4dc5e5369ce9d6f0b354bb936090eb357
Himanshu Tripathi <66693732+HimanskDevStuff@users.noreply.github.com>
2021-05-08 19:01:35 +0530

    Add files via upload

---
+++ NavigationComponentActionBarMenu/app/src/main/java/com/himanshu/navigationcomponent/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.himanshu.navigationcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController : NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //find navHostFragment from layout
+        val navHostFrament = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        //find navController
+        navController = navHostFrament.findNavController() //or   navHostFrament.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item_actionbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(item.itemId == R.id.termsAndCondition){
+            val action = NavGraphDirections.actionGlobalTermsAndCondition()
+            navController.navigate(action)
+            true
+        }
+        else item.onNavDestinationSelected(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
